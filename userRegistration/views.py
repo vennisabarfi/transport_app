e37c01a0877844de33333dbdbb7c01a5612d40db
veyhn <109932151+veyhn@users.noreply.github.com>
2024-06-04 10:48:21 -0400

    building views

--- userRegistration/views.py
+++ userRegistration/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django import HttpResponse
 
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse("Youre looking st this")
