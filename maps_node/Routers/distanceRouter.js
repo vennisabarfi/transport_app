563b7d6f7f4b19b3309918ed2db2d943e1baa582
veyhn <109932151+veyhn@users.noreply.github.com>
2024-06-10 15:25:47 -0400

    Update to distanceRouter and environment variables

--- maps_node/Routers/distanceRouter.js
+++ maps_node/Routers/distanceRouter.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 const fs  = require('fs');
 const dotenv = require('dotenv');
-require('dotenv').config({path: path.resolve(__dirname,'../.env_secret_api_key')});
+// require('dotenv').config({ path: require('find-config')('.env_secret_api_key') }) //read api key
 const path = require('path');
+const ck = require('ckey');
+const apiKey = ck.API_KEY; // read api key from .env file
+
 
 // //Read api key from secret file
 // const apiKey = fs.readFile('.env_secret_api_key', function(err,data){
@@ -13,25 +16,21 @@ const path = require('path');
 // });
 
 try {
-    const apiKey = process.env.API_KEY;
     console.log(apiKey);
 } catch (error) {
     console.log(error);
 }
-
-
  
-
 //look up locations (autocomplete feature included). 
 router.get('/location', function(_req,res){
     const location_query = req.query.name;
     const baseURL = 'api.tomtom.com';
     const versionNumber = 2;
-    const Your_API_Key = ''; //add api key here 
     const language = 'en-US';
+    const response_format = 'json';
         const config ={
             method: 'get',
-            url: `https://${baseURL}/search/${versionNumber}/autocomplete/${location_query}.{ext}?key=${Your_API_Key}&language=${language}`,
+            url: `https://${baseURL}/search/${versionNumber}/autocomplete/${location_query}.${response_format}?key=${apiKey}&language=${language}`,
             headers:{
              
             }
